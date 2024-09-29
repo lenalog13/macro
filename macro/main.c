431191f412563fe7a25227b3c7870eef7c6dd428
«lenalog13» <lenaloginova2000@mail.ru>
2024-09-29 16:51:53 +0300

    Initial Commit

---
+++ macro/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  macro
+//
+//  Created by Елена Логинова on 29.09.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
